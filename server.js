46cb10c8236183fc6eb1d902a7e97db5370dd35d
Venkat Koushik <144405842+vkoushik15@users.noreply.github.com>
2025-02-13 09:27:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const wtrroutes = require('./routes/wtrroutes');
 app.use(express.json());
 const cors = require('cors');   
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://finwiz-c5772.web.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 };
